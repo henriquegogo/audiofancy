97ef9546469cde8fad6c27209b0c58e112994df1
Henrique Gogó <henriquegogo@gmail.com>
2016-01-12 13:33:27 -0300

    Detecting keyboard events and playing audio. To exit, press period

--- main.c
+++ main.c
@@ -24,17 +24,17 @@ void deinitialize() {
 
 int main(int argc, char const *argv[]) {
   initialize();
-  // play("./test.wav");
-  // SDL_Delay(1000);
-  // play("./test.wav");
-  // SDL_Delay(1000);
-  // play("./test.wav");
-  // SDL_Delay(3000);
 
-  for(;;) {
-    int key = getchar();
-    printf("You clicked on: %i\n", key);
+  int key;
+
+  system("/bin/stty raw");
+  while (key = getchar() != '.') {
+    play("./test.wav");
+    printf("%c\n\r", key);
   }
+  system("/bin/stty cooked");
+
+  printf("Bye bye!\n");
 
   deinitialize();
 
